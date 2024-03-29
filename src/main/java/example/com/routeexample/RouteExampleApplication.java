03f4dd338f45d1c64d4b48d25f12fcd436f6b2f2
leeyejin <leeyejin113@gmail.com>
2024-03-29 13:52:17 +0900

    Initial commit

---
+++ src/main/java/example/com/routeexample/RouteExampleApplication.java
@@ -0,0 +1,13 @@
+package example.com.routeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/com/routeexample/RouteExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package example.com.routeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
